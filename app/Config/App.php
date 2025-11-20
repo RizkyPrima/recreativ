71143b58447e9d0ee104c4e856b5b13b48668df1
Rizky Prima <64130030+RizkyPrima@users.noreply.github.com>
2025-11-20 18:21:21 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,9 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public $baseURL = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https" : "http")
-    . "://"
-    . $_SERVER['HTTP_HOST'] . "/";
+     public $baseURL = '';
 
 
     /**
