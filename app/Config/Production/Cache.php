b13a856371cb94617c3da125272d31588abe8be7
Rizky Prima <64130030+RizkyPrima@users.noreply.github.com>
2025-11-22 13:48:55 +0700

    Create Cache.php

---
+++ app/Config/Production/Cache.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Config\Production;
+
+use CodeIgniter\Config\BaseConfig;
+use CodeIgniter\Cache\Handlers\DummyHandler;
+
+class Cache extends BaseConfig
+{
+    /**
+     * Use 'dummy' on serverless (Vercel) because filesystem is read-only.
+     */
+    public string $handler = 'dummy';
+
+    public string $backupHandler = 'dummy';
+
+    public string $storePath = '';
+
+    public $cacheQueryString = false;
+
+    public string $prefix = '';
+
+    public int $ttl = 60;
+
+    public string $reservedCharacters = '{}()/\@:';
+
+    public array $file = [
+        'storePath' => '',
+        'mode'      => 0640,
+    ];
+
+    public array $validHandlers = [
+        'dummy' => DummyHandler::class,
+    ];
+}
